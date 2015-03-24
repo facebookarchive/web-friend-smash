4c3c5f1806b955262634f20edf478997a463f16c
Connor Treacy <connortreacy@gmail.com>
2015-03-24 21:41:49 +0000

    Implementation using Facebook PHP SDK

--- payments/callback.php
+++ payments/callback.php
@@ -1,7 +1,27 @@
 <?php
 
+// Skip these two lines if you're using Composer
+define('FACEBOOK_SDK_V4_SRC_DIR', 'facebook-php-sdk/src/Facebook/');
+require __DIR__ . '/facebook-php-sdk/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphObject;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication('844042765624257','qqJJ0AfOPpXQtUymZpkj4HPgon8');
+
 $verify_token = "friendsmash";
 $app_token = "844042765624257|qqJJ0AfOPpXQtUymZpkj4HPgon8";
+
+// Use one of the helper classes to get a FacebookSession object.
+//   FacebookRedirectLoginHelper
+//   FacebookCanvasLoginHelper
+//   FacebookJavaScriptLoginHelper
+// or create a FacebookSession with a valid access token:
+$session = new FacebookSession($app_token);
+
+// Get the GraphUser object for the current user:
 
 $method = $_SERVER['REQUEST_METHOD'];
 //stdout('RTU');
@@ -10,24 +30,35 @@ if ($method == 'GET' && $_GET['hub_verify_token'] === $verify_token) {
 } else {
     $data = file_get_contents("php://input");
     $json = json_decode($data, true);
+
     if( $json["object"] && $json["object"] == "payments" ) {
-        
-        $graph_url = "https://graph.facebook.com/" . $json["entry"][0]["id"] . "?fields=actions,items,gift_requests&access_token=" . $app_token;
-        error_log("graph api");
-        error_log($graph_url);
+        $payment_id = $json["entry"][0]["id"];
+        try {
+            $result = (new FacebookRequest(
+                $session, 'GET', '/' . $payment_id . '?fields=user,actions,items,gift_requests'
+            ))->execute()->getGraphObject(GraphObject::className());
+            error_log('payment id: '.$result->getProperty('id'));
+            $actions = $result->getPropertyAsArray('actions');
+            if( $actions[0]->getProperty('status') == 'completed' ){
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $graph_url);
-        $result = curl_exec($curl);
-        error_log("result");
-        error_log($graph_url);
-
-        $result_json = json_decode($result, true);
-
-        if($result_json){// && $result_json["actions"][0] && $result_json["actions"][0]["status"] = "completed") {
-            error_log($result_json);
-        } else {
-            error_log($result);
+                $gift_requests = $result->getProperty('gift_requests');
+                $user = $result->getProperty('user');
+                $recipient = '';
+                
+                if( $gift_requests ) {
+                    $data = $gift_requests->getPropertyAsArray('data');
+                    $recipient = $data[0]->getProperty('to')->getProperty('id');
+                    error_log('gift to '.$recipient);
+                } else {
+                    $recipient = $user->getProperty('id');
+                    error_log('purchase for '.$recipient);
+                }
+            }
+        } catch (FacebookRequestException $e) {
+          // The Graph API returned an error
+            error_log($e->getRawResponse());
+        } catch (\Exception $e) {
+            error_log($e);
         }
     }
 }
